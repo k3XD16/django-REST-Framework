3f51a2b50c48df755fa45b73087224e9c1f5877d
Mohamed Khasim <Khasimrockers@gmail.com>
2024-03-11 09:24:52 +0530

    REST_Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,45 +1,19 @@
-from django.shortcuts import render
 from django.forms.models import model_to_dict
-from django.http import JsonResponse , HttpResponse
-import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
 # Create your views here.
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
     
+    # if request.method == ['POST']:
+    #     return Response({'detail': 'Method "GET" not allowed.'}, status=405)
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data)
-        # print(data)
-    return JsonResponse(data)
+        data = model_to_dict(model_data,fields=['id', 'brand', 'model'])
+    return Response(data)
         
-    #     print(data)
-    #     data =dict(data)
-    #     json_data = json.dumps(data)
-    # return HttpResponse(json_data , headers={"content-Type":"application/json"})
-    
-    
-    # print(request.GET)
-    # print(request.POST)
-    # body = request.body
-    # data = {}
-    # try:
-    #     data = json.loads(body)
-    # except:
-    #     pass
-    # print(data)
-    # print(request.headers)
-    # # json.dumps(request.headers)
-    # data['params'] = request.GET
-    # data['Headers'] = dict(request.headers)
-    # data['content_type'] = request.content_type
-    
-    # return JsonResponse(
-    #     {"greet":"Welcome to Django REST Framework API!!",
-    #      "name":"hello",
-    #      "age": 25,
-    #      "version": "v1.0.1",
-    #     })
-    
--- py_client/main.py
+++ py_client/main.py
@@ -8,5 +8,5 @@ res = requests.get(endpoints , params={"abc": 212},json={"hi": "Hello world"})
 
 # print(res.headers)
 # print(res.text)
-# print(res.status_code)
 print(res.json())
+print(res.status_code)
