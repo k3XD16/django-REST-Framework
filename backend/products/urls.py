e420275ac65e6ceb70c56f1885f3373a5d800cbb
Mohamed Khasim <Khasimrockers@gmail.com>
2024-03-13 13:08:42 +0530

    Authentication & Permissions #01

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 # api/products/
 urlpatterns = [
-    path('',views.product_mixin_view),
+    path('',views.product_list_create_view),
     path('list/',views.product_list_mixin),
-    path('<int:pk>/',views.product_retrieve_mixin),
+    path('<int:pk>/',views.product_list_create_view),
     path('<int:pk>/update/',views.product_update_mixin),
     path('<int:pk>/delete/',views.product_destroy_mixin),
     
